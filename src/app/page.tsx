e58e16378897d8a5684d336747e4d8352578afcc
lutzzdias <thiagolutzdias@hotmail.com>
2023-05-17 13:54:22 -0400

    style: rearrange tailwind classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="flex h-screen flex-col">
       <div className="flex flex-1">
         <Sidebar />
-        <main className="flex-1 p-6">
+        <main className="flex-1 px-6 py-4">
           <div className="flex items-center gap-4">
             <button className="rounded-full bg-black/40 p-1">
               <ChevronLeft />
@@ -18,12 +18,12 @@ export default function Home() {
             </button>
           </div>
 
-          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+          <h1 className="mt-10 text-3xl font-semibold">Good Afternoon</h1>
 
-          <div className="grid grid-cols-3 gap-4 mt-4">
+          <div className="mt-4 grid grid-cols-3 gap-4">
             <a
               href=""
-              className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+              className="group flex items-center gap-4 overflow-hidden rounded bg-white/5 transition-colors hover:bg-white/10"
             >
               <Image
                 src="/album.png"
@@ -32,13 +32,13 @@ export default function Home() {
                 height={104}
               />
               <strong>Astronaut</strong>
-              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <button className="invisible ml-auto mr-8 flex h-12 w-12 items-center justify-center rounded-full bg-green-400 pl-1 text-black group-hover:visible">
                 <Play />
               </button>
             </a>
             <a
               href=""
-              className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+              className="group flex items-center gap-4 overflow-hidden rounded bg-white/5 transition-colors hover:bg-white/10"
             >
               <Image
                 src="/album.png"
@@ -47,13 +47,13 @@ export default function Home() {
                 height={104}
               />
               <strong>Astronaut</strong>
-              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <button className="invisible ml-auto mr-8 flex h-12 w-12 items-center justify-center rounded-full bg-green-400 pl-1 text-black group-hover:visible">
                 <Play />
               </button>
             </a>
             <a
               href=""
-              className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+              className="group flex items-center gap-4 overflow-hidden rounded bg-white/5 transition-colors hover:bg-white/10"
             >
               <Image
                 src="/album.png"
@@ -62,13 +62,13 @@ export default function Home() {
                 height={104}
               />
               <strong>Astronaut</strong>
-              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <button className="invisible ml-auto mr-8 flex h-12 w-12 items-center justify-center rounded-full bg-green-400 pl-1 text-black group-hover:visible">
                 <Play />
               </button>
             </a>
             <a
               href=""
-              className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+              className="group flex items-center gap-4 overflow-hidden rounded bg-white/5 transition-colors hover:bg-white/10"
             >
               <Image
                 src="/album.png"
@@ -77,13 +77,13 @@ export default function Home() {
                 height={104}
               />
               <strong>Astronaut</strong>
-              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <button className="invisible ml-auto mr-8 flex h-12 w-12 items-center justify-center rounded-full bg-green-400 pl-1 text-black group-hover:visible">
                 <Play />
               </button>
             </a>
             <a
               href=""
-              className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+              className="group flex items-center gap-4 overflow-hidden rounded bg-white/5 transition-colors hover:bg-white/10"
             >
               <Image
                 src="/album.png"
@@ -92,13 +92,13 @@ export default function Home() {
                 height={104}
               />
               <strong>Astronaut</strong>
-              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <button className="invisible ml-auto mr-8 flex h-12 w-12 items-center justify-center rounded-full bg-green-400 pl-1 text-black group-hover:visible">
                 <Play />
               </button>
             </a>
             <a
               href=""
-              className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+              className="group flex items-center gap-4 overflow-hidden rounded bg-white/5 transition-colors hover:bg-white/10"
             >
               <Image
                 src="/album.png"
@@ -107,18 +107,18 @@ export default function Home() {
                 height={104}
               />
               <strong>Astronaut</strong>
-              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <button className="invisible ml-auto mr-8 flex h-12 w-12 items-center justify-center rounded-full bg-green-400 pl-1 text-black group-hover:visible">
                 <Play />
               </button>
             </a>
           </div>
 
-          <h2 className="font-semibold text-2xl mt-10">Made for Thiago Dias</h2>
+          <h2 className="mt-10 text-2xl font-semibold">Made for Thiago Dias</h2>
 
-          <div className="grid grid-cols-8 gap-4 mt-4">
+          <div className="mt-4 grid grid-cols-8 gap-4">
             <a
               href=""
-              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+              className="flex flex-col gap-2 rounded-md bg-white/5 p-3 hover:bg-white/10"
             >
               <Image
                 className="w-full"
@@ -134,7 +134,7 @@ export default function Home() {
             </a>
             <a
               href=""
-              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+              className="flex flex-col gap-2 rounded-md bg-white/5 p-3 hover:bg-white/10"
             >
               <Image
                 className="w-full"
@@ -150,7 +150,7 @@ export default function Home() {
             </a>
             <a
               href=""
-              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+              className="flex flex-col gap-2 rounded-md bg-white/5 p-3 hover:bg-white/10"
             >
               <Image
                 className="w-full"
@@ -166,7 +166,7 @@ export default function Home() {
             </a>
             <a
               href=""
-              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+              className="flex flex-col gap-2 rounded-md bg-white/5 p-3 hover:bg-white/10"
             >
               <Image
                 className="w-full"
@@ -182,7 +182,7 @@ export default function Home() {
             </a>
             <a
               href=""
-              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+              className="flex flex-col gap-2 rounded-md bg-white/5 p-3 hover:bg-white/10"
             >
               <Image
                 className="w-full"
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,7 +6,7 @@ import {
 
 export function Sidebar() {
   return (
-    <aside className="w-72 bg-zinc-950 p-6">
+    <aside className="w-72 bg-zinc-950 py-4">
       <nav className="space-y-5">
         <a
           href=""
@@ -30,7 +30,7 @@ export function Sidebar() {
           Your Library
         </a>
       </nav>
-      <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+      <nav className="mt-6 flex flex-col gap-3 border-t border-zinc-800 pt-6">
         <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
           Hot Hits Brasil
         </a>
